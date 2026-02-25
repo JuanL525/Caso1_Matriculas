26dcf51debab84fbac0435ac0d844a9b6d242a80
JuanL525 <juan.lucero@epn.edu.ec>
2026-02-25 15:35:04 -0500

    para netlify

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import matrciulaRoutes from './routes/matriculaRoutes.js'
 
 const app = express();
 
-const whitelist = [ 'http://127.0.0.1:5501', 'http://localhost:5501', 'http://127.0.0.1:4000', 'http://localhost:4000' ];
+const whitelist = [ 'http://127.0.0.1:5501', 'http://localhost:5501', 'http://127.0.0.1:4000', 'http://localhost:4000', 'https://caso1-matriculas-frontdend.netlify.app/' ];
 
 const corsOptions = {
     origin: function (origin, callback) {
